93c6cc943a868b5977aa01b4dd7f9f99edc0f29c
Iskander <isk.dev001@gmail.com>
2025-04-06 16:27:34 +0500

    implement pwa

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,39 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      vue(),
-      tailwindcss()
-  ],
+    plugins: [
+        vue(),
+        tailwindcss(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true,  // Включаем PWA в режиме разработки
+            },
+            manifest: {
+                name: 'Nerdify',
+                short_name: 'Nerdify',
+                description: 'Nerdify text lmao',
+                theme_color: '#be9851',
+                background_color: '#ffffff',
+                display: 'standalone',
+                start_url: '.',
+                icons: [
+                    {
+                        src: '/icons/android-icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/icons/android-icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    }
+                ]
+            }
+        })
+    ],
 })
