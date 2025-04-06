d388184401e81fdecb804bf98c00128a9afb8f97
Iskander <isk.dev001@gmail.com>
2025-04-06 10:27:40 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut illum laborum neque quidem sequi temporibus voluptatum! Accusamus atque, dolore dolorum hic illum, incidunt magni neque nobis perspiciatis recusandae rem veritatis?</p>
+</template>
